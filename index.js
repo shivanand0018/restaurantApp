65ed1a114134e67b6a4b7c94c2a2d1568b26e021
shivanand0018 <101448498+shivanand0018@users.noreply.github.com>
2023-03-01 09:25:11 +0530

    changes made

--- index.js
+++ index.js
@@ -1,45 +1,65 @@
-function addDish(e) {
-
-    e.preventDefault();
-    let data = {
-        dishname: e.target[0].value,
-        price: e.target[1].value,
-        tableNo: e.target[2].value
+async function addDish(event) {
+    try {
+        event.preventDefault();
+        let data = {
+            dishname: event.target.dishName.value,
+            price: event.target.price.value,
+            tableNo: event.target.tableNo.value
+        }
+        let post = await axios.post(`https://crudcrud.com/api/2c02f652278044119e446d204851f825/data`, data);
+        showData(post.data);
     }
-    axios.post(`http://localhost:3000/data`, data).then(() => {
-        alert(`Dish added to ${tableNo}`)
-    })
+    catch (err) {
+        console.log('error message:' + err.name);
+    }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get(`http://localhost:3000/data`).then((res) => {
-        for (let i = 0; i < res.data.length; i++) {
-            console.log(res.data);
-            let list = document.getElementById(res.data[i].tableNo);
-            let btn=document.createElement('button')
-            let list1 = document.createElement('ul');
-            btn.appendChild(document.createTextNode('Delete'))
-            btn.onclick=(()=>{
-                console.log(res.data[i].id);
-                axios.delete(`http://localhost:3000/data/${res.data[i].id}`).then(()=>{
-
-                })
-            })
-            if (res.data[i].tableNo === "Table 1") {
-                list1.textContent = res.data[i].dishname + "-" + res.data[i].price
-                list1.appendChild(btn);
-                list.appendChild(list1);
-            }
-            else if (res.data[i].tableNo == "Table 2") {
-                list1.textContent = res.data[i].dishname + "-" + res.data[i].price
-                list1.appendChild(btn);
-                list.appendChild(list1);
-            }
-            else if (res.data[i].tableNo == "Table 3") {
-                list1.textContent = res.data[i].dishname + "-" + res.data[i].price
-                list1.appendChild(btn);
-                list.appendChild(list1);
+    async function getData() {
+        try {
+            let res = await axios.get(`https://crudcrud.com/api/2c02f652278044119e446d204851f825/data`);
+            for (let i = 0; i < res.data.length; i++) {
+                showData(res.data[i]);
             }
         }
-    })
+        catch (err) {
+            console.log(err);
+        }
+    }
+    getData();
 })
+
+function showData(data) {
+    try {
+        let list = document.getElementById(data.tableNo);
+        let btn = document.createElement('button');
+        let list1 = document.createElement('ul');
+        list1.id = `${data._id}`;
+        console.log(list1);
+        btn.appendChild(document.createTextNode('Delete'));
+        btn.onclick = (() => {
+            deleteData(data);
+        })
+        list1.textContent = data.dishname + "-" + data.price + " ";
+        list1.appendChild(btn);
+        list.appendChild(list1);
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+async function deleteData(data) {
+    try {
+        let res = await axios.delete(`https://crudcrud.com/api/2c02f652278044119e446d204851f825/data/${data._id}`)
+        if (res.status == 200) {
+            alert('Deleted successfully');
+            let parent = document.getElementById(data.tableNo);
+            let child = document.getElementById(data._id)
+            parent.removeChild(child);
+        }
+    }
+    catch (err) {
+        console.log(err.name);
+    }
+}
